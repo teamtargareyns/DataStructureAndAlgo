286dcce08d967780f6b5bc0d834aada339c963aa
YATRAONLINE\aakash.singh1 <aakash.singh1@yatra.com>
2019-05-20 22:43:39 +0530

    [PascalTriangle_final_fix]

--- src/com/test/aks/data_structure/interview_bit/arrays/PascalTriangle.java
+++ src/com/test/aks/data_structure/interview_bit/arrays/PascalTriangle.java
@@ -1,9 +1,14 @@
 package com.test.aks.data_structure.interview_bit.arrays;
 
+import java.util.ArrayList;
+
 public class PascalTriangle {
 
 
+
     /**
+     * Solution perfect for jdk7 but the issue is we are having extra zeros at the end of each row
+     * with this solution
      * @param n
      * @return
      */
@@ -40,10 +45,72 @@ public class PascalTriangle {
         return arr;
     }
 
+    /**
+     * Solution perfectly suitable for INterviewBIt.com
+     * @param n
+     * @return
+     */
+    private static ArrayList<ArrayList<Integer>> printPascalTriangleArraylist(int n) {
+        ArrayList<ArrayList<Integer>> outerArraylist = new ArrayList<>();
+        ArrayList<Integer> innerList;
+
+        for (int row = 0; row < n; ++row) {
+            innerList = new ArrayList<>();
+            outerArraylist.add(innerList);
+
+            for (int col = 0; col < row + 1; ++col) {
+                //first and last element in the row
+                if (col == 0 || col == row) {
+                    //arr[row][col] = 1;
+                    innerList.add(1);
+                } else {
+                    //middle elements in the row
+                    //arr[row][col] = arr[row - 1][col] + arr[row - 1][col - 1];
+
+                    //middle elements in the row
+                    outerArraylist.get(row).add(col,outerArraylist.get(row - 1).get(col - 1) + outerArraylist.get(row - 1).get(col));
+
+                    //check if value is more than 10
+                    /*if (arr[row][col] >= 10) {
+                        int quotient = arr[row][col] / 10;
+                        int remainder = arr[row][col] % 10;
+                        arr[row][col] = remainder;
+                        arr[row][col - 1] = arr[row][col - 1] + quotient;
+                    }*/
+
+                    /**
+                     * v.IMP.
+                     */
+                    //FOR MAKING IT PERFECT EXPONENT OF 11 SOLUTION
+                    //check if value is more than 10
+                    /*if (outerArraylist.get(row).get(col) >= 10) {
+                        Integer quotient = outerArraylist.get(row).get(col) / 10;
+                        Integer remainder = outerArraylist.get(row).get(col) % 10;
+                        outerArraylist.get(row).set(col,remainder);
+                        outerArraylist.get(row).set(col-1,outerArraylist.get(row).get(col-1)+quotient);
+                    }*/
+
+                }
+            }
+
+            //outerArraylist.add(innerList);
+        }
+
+
+        for (int i = 0; i < outerArraylist.size(); i++) {
+            for (int j = 0; j < outerArraylist.get(i).size(); j++) {
+                System.out.print(outerArraylist.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+
+        return outerArraylist;
+    }
+
 
     // Driver program
     public static void main(String[] args) {
-        int n = 5;
-        printPascalTriangle(n);
+        int n = 9;
+        printPascalTriangleArraylist(n);
     }
 }
