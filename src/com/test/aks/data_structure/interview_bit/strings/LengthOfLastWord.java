4d994b9fbf324f9a0bf1f9e2f9acb1885f13c9fb
YATRAONLINE\aakash.singh1 <aakash.singh1@yatra.com>
2019-06-07 16:58:59 +0530

    [LengthOfLastWord]

--- src/com/test/aks/data_structure/interview_bit/strings/LengthOfLastWord.java
+++ src/com/test/aks/data_structure/interview_bit/strings/LengthOfLastWord.java
@@ -2,5 +2,32 @@ package com.test.aks.data_structure.interview_bit.strings;
 
 public class LengthOfLastWord {
 
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public static int lengthOfLastWord(final String str) {
+        String localStr = str;
+        int size = str.length();
+        if(size==0){
+            return 0;
+        }
 
+        //trim leading and trailing spaces
+        //ASCII VALUE OF SPACE - 32
+        localStr = localStr.trim();
+        int count=0;
+        for(int i=localStr.length()-1; i>=0; i--){
+            if(localStr.charAt(i) != 32){
+                count++;
+                continue;
+            }else {
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String myString = "Hello World";
+        int result = lengthOfLastWord(myString);
+        System.out.println(result);
+    }
 }
