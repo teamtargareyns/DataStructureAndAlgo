0dc17deabb848d21ad5708da7b45710d82ce5dd0
YATRAONLINE\aakash.singh1 <aakash.singh1@yatra.com>
2019-06-10 14:25:10 +0530

    [ReverseString]

--- src/com/test/aks/data_structure/interview_bit/strings/ReverseString.java
+++ src/com/test/aks/data_structure/interview_bit/strings/ReverseString.java
@@ -2,16 +2,22 @@ package com.test.aks.data_structure.interview_bit.strings;
 
 public class ReverseString {
 
-    static String reverseString(String str){
-
-
-
-        return null;
+    private static String reverseString(String str) {
+        //trim leading and trailing space
+        str = str.trim();
+        String[] strArr = str.split("\\s");
+        String newString = "";
+        for (int i = strArr.length - 1; i >= 0; i--) {
+            newString = newString.concat(strArr[i]);
+            if(i>0){
+                newString = newString.concat(" ");
+            }
+        }
+        return newString;
     }
 
     public static void main(String[] args) {
         String str = "My name is Aakash";
-
         System.out.println(reverseString(str));
     }
 }
